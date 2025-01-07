e39de199240503748cd96c8792495f717ab56105
Semih <semhavc357@gmail.com>
2025-01-07 23:18:39 +0300

    added connectivity

---
+++ client/src/screens/index.js
@@ -0,0 +1,23 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import SingUpPage from "./SingUpPage";
+import GamePage from "./GamePage";
+import LobiPage from "./LobiPage";
+import SecondPage from "./SecondPage";
+import TakeWordPage from "./TakeWordPage";
+import OpponentPage from "./OpponentPage";
+import ScorePage from "./ScorePage";
+
+export {
+    HomePage,
+    LoginPage,
+    ProfilePage,
+    SingUpPage,
+    GamePage,
+    LobiPage,
+    SecondPage,
+    TakeWordPage,
+    OpponentPage,
+    ScorePage,
+}
